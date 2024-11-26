6e42165009aa70f6545c728181f600b0cf563647
keelfy <keelfy@gmail.com>
2024-11-26 19:03:55 +0100

    Removed CORS restrictions

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,10 +9,10 @@ import (
 func CORS(handler http.Handler) http.Handler {
 	// Setup CORS middleware
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},         // Allow frontend origin
-		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},        // Allow these methods
-		AllowedHeaders:   []string{"Content-Type", "Authorization"}, // Allow custom headers
-		AllowCredentials: true,                                      // Allow credentials like cookies, tokens
+		AllowedOrigins:   []string{"*"},                      // Allow frontend origin
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"}, // Allow these methods
+		AllowedHeaders:   []string{"*"},                      // Allow custom headers
+		AllowCredentials: true,                               // Allow credentials like cookies, tokens
 	})
 
 	return c.Handler(handler)

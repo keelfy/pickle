acd184f1b134be7a8235f74f40d89723ee798d2a
keelfy <keelfy@gmail.com>
2025-02-10 21:01:51 +0100

    only profile owner can create collections

--- app/(view)/[link]/collections-section.tsx
+++ app/(view)/[link]/collections-section.tsx
@@ -2,6 +2,7 @@ import CollectionLine from "@/components/view/content-collections/collection-lin
 import ContentCollection from "@/components/view/content-collections/content-collection";
 import CreateCollectionButton from "@/components/view/content-collections/create-collection-button";
 import { fetchCollections, fetchProfileByLink } from "@/hooks/api-endpoints-server";
+import getUser from "@/hooks/getUser";
 
 type Props = {
     params: Promise<{ link: string }>;
@@ -9,7 +10,10 @@ type Props = {
 
 export default async function CollectionsSection({ params }: Props) {
     const { link } = await params;
+
     const profile = await fetchProfileByLink(link);
+    const user = await getUser();
+    const isUserAuthorized = profile?.id === user?.id;
     const collections = await fetchCollections(profile);
 
     return (
@@ -17,7 +21,7 @@ export default async function CollectionsSection({ params }: Props) {
             {collections.map((collection) => (
                 <ContentCollection key={collection.id} collection={collection} />
             ))}
-            <CollectionLine leftSide={<CreateCollectionButton />} />
+            {isUserAuthorized && <CollectionLine leftSide={<CreateCollectionButton />} />}
         </>
     )
 }

e359b7a79cea9851a116c99f1fa75523c3943199
keelfy <keelfy@gmail.com>
2026-03-08 12:42:09 +0100

    fixed next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,7 +4,7 @@ const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: NODE_ENV === 'production' ? 'standalone' : undefined,
+  output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
   images: {
     remotePatterns: [
       { hostname: 'flowbite.com' },
